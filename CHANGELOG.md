9ef565632d9edc41d7b5f515965b744c60a5c495
HarshitJoshi9152 <harsjoshi1614@gmail.com>
2021-04-24 01:05:57 +0530

    implementing comments ...

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,4 @@
 ## CHANGELOG
 
-1. tried to add text-to-speech.
-2. worked on focus mode components.
-3. removed full screen mode.
-4. fixed overlapping on posts in stack mode.
-5. got rid of moderator messages.
-6. displayMode is set after we have the data to avoid errors.
-   // if we set the data before we have the data the other components try to render using the data which results in errors.
+1. tried to implement the focus mode with comments but ran into a recursion error trying to render the comments.
+2. moved mod post check to Thoughts component to avoid partial components rendering.
--- src/components/Thoughts.js
+++ src/components/Thoughts.js
@@ -6,11 +6,13 @@ import "./thoughts.css";
 import StackGrid from "react-stack-grid";
 
 export default function Thoughts(props) {
+	const TESTINGMODE = "focus";
+	let [dataReceived, setDataReceived] = useState(false);
 	const validModes = ["focus", "stack"];
 	const [displayMode, setDisplayMode] = React.useState("");
 	const [postsData, setPostsData] = React.useState([]);
-
-	const screenRef = useRef();
+	// comments will be loaded by children components.
+	const [comments, setComments] = React.useState([{}]);
 
 	// fetching the data on mount;
 	React.useEffect(() => {
@@ -21,24 +23,51 @@ export default function Thoughts(props) {
 			.then((res) => (res.status !== 200 ? 1 : res.json()))
 			.then((json) => {
 				console.log(json.data.children);
-				setPostsData(json.data.children);
+				setPostsData(json.data.children.slice(1));
 				// if we set the data before we have the data the other components try to render using the data which results in errors.
-				setDisplayMode("stack");
-			});
+				setDisplayMode(TESTINGMODE);
+				setDataReceived(true);
+			})
+			.catch(console.log);
 	}, []);
 
+	const findComment = (url) => comments.filter((val) => val.url === url);
+
+	const getComments = async (postUrl) => {
+		let foundCom = findComment(postUrl);
+		if (foundCom.length !== 0) return foundCom;
+
+		// if comments are not in comments fetch them;
+		const res = await fetch(postUrl);
+		const c = await res.json();
+		console.log("HEHEHEHEH");
+		console.log(c);
+		let url = c[0].data.children[0].data.title; // | id | subreddit_id | title | permalink | url;
+		// kind: "listing" | "t1" | "t3"
+		let comObj = {
+			url: [url],
+			comments: c[1].data.children.map((obj) => obj.data).slice(0, 2)
+		};
+		setComments((coms) => coms.concat([comObj]));
+		return comObj;
+	};
+
 	return (
-		<div className="viewarea" ref={screenRef}>
+		<div className="viewarea">
 			{/*should we add a powerbar here to control the view styles etc ?? */}
 			{displayMode === "stack" && (
 				<StackGrid columnWidth={300}>
 					{postsData &&
+						// this doesnt need any initial no data phase protection coz postsData has 0 elements at that time
 						postsData.map((post, i) => (
 							<Post key={i} {...post.data}></Post>
 						))}
 				</StackGrid>
 			)}
-			{displayMode === "focus" && <Focus postsData={postsData} />}
+			{/*when i used the useRef hook to store dataReceived it didnt work coz after being set to true it did not cause a re-render */}
+			{displayMode === "focus" && dataReceived && (
+				<Focus postsData={postsData} getComments={getComments} />
+			)}
 		</div>
 	);
 }
@@ -54,8 +83,8 @@ function Post({
 	distinguished,
 	displayMode = "stack"
 }) {
-	// to avoid rendering moderator posts.
-	if (distinguished === "moderator") return null;
+	// to avoid rendering moderator posts. todo: PUT THIS IN focus mode?
+	// if (distinguished === "moderator") return null;
 
 	return (
 		<div className="post">
@@ -77,25 +106,10 @@ function Post({
 	// score, total_awards_received
 }
 
-// return (
-// 	<div>
-// 		<Post></Post>
-// 		{/*
-// 			search
-// 			top
-// 			hot
-// 			new
-// 			viewstyle -> compact | detailed
-
-// 		*/}
-// 	</div>
-// );
-
-/**/
-const Focus = ({ postsData }) => {
-	console.log(postsData);
+const Focus = ({ postsData, getComments }) => {
 	const [currentPost, setCurrentPost] = useState(0);
 	const [currentComments, setCurrentComments] = useState([]);
+	// currentComments : {url:[url], comments: obj `children[]]`}
 	const currentPostData = postsData[currentPost].data;
 
 	const nextPost = (e) => {
@@ -105,32 +119,42 @@ const Focus = ({ postsData }) => {
 
 	useEffect(() => {
 		// Load Comments
-		// let curl = `${currentPostData.url}.json`;
-		let curl = `${"https://www.reddit.com/r/Showerthoughts/comments/mw2amn/having_to_attend_a_wedding_you_dont_want_to_sucks/"}.json`;
-		fetch(curl)
-			.then((res) => res.json())
-			.then(console.log)
-			.catch(console.log);
+		// let curl = `${"https://www.reddit.com/r/Showerthoughts/comments/mw2amn/having_to_attend_a_wedding_you_dont_want_to_sucks/"}.json`;
+		let curl = `${currentPostData.url}.json`;
+		getComments(curl).then((comObj) => {
+			setCurrentComments(comObj.comments);
+			console.log(comObj);
+			debugger;
+		});
 		// chance to delete comments from memory when post changes ?...
-	}, [currentPostData]);
+	}, [currentPostData, currentPost]);
 
 	return (
 		<>
 			<Post {...currentPostData} />
 			{/*<Comments/>*/}
-			{/*<Comments data={currentComments}/>*/}
+			<div className="comments">
+				{currentComments &&
+					currentComments.map((commentObj) => (
+						<Comment data={commentObj} />
+					))}
+			</div>
 		</>
 	);
 };
 
-// const Comments = ({ data }) => {
-// 	return (
-// 		<div>
-// 			{data.map((com) => (
-// 				<Comment data={commentObj} />
-// 			))}
-// 		</div>
-// 	);
-// };
+const Comment = ({ data, ml = 0 }) => {
+	const styles = { marginLeft: `${ml}px` };
+	// Comment should be a recursive component.
+	return (
+		<div className="comment">
+			<p style={styles}> {data.body} </p>
+		</div>
+	);
+};
 
-/**/
+// {data.replies.data.children.map((replyData) => {
+// 	// replyData is a standard comment Obj
+// 	return <Comment data={replyData.data} ml={ml + 2}></Comment>;
+// 	// {data.replies && <Comment data={data.replies.data.children}></Comment>}
+// })}
