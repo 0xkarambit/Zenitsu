e767b75f9f9e510b7d9f53085685607c1f6ab744
HarshitJoshi9152 <harsjoshi1614@gmail.com>
2021-05-25 14:38:30 +0530

    added keb shortcuts to login components

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
+import { useHotkeys } from "react-hotkeys-hook";
 
 import { VscAccount } from "react-icons/vsc";
 
@@ -15,16 +16,14 @@ const LoginButton = ({ loggedIn, setLoggedIn }) => {
 	// if logged in show user profile pic instead.
 	const [hide, setHide] = useState(true);
 
+	useHotkeys("l", (e) => setHide(false));
+
 	const clicked = () => {
 		setHide((show) => !show);
 	};
 
-	const submit = () => {
-		alert("LOGGGIN IN !");
-	};
-
 	return (
-		<span className="login-button">
+		<span>
 			{!loggedIn && (
 				<VscAccount
 					onClick={clicked}
@@ -32,25 +31,44 @@ const LoginButton = ({ loggedIn, setLoggedIn }) => {
 					title="Sign In"
 				/>
 			)}
-			{!hide && <LoginPopup {...{ setHide, submit }} />}
+			{!hide && <LoginPopup setHide={setHide} />}
 		</span>
 	);
 };
 
 export default LoginButton;
 
-const LoginPopup = ({ setHide, submit }) => {
-	const [popup, user] = [useRef(), useRef()];
+const LoginPopup = ({ setHide }) => {
+	// used to focus on username input field and to close popup on outside click.
+	const [popup, userInput] = [useRef(), useRef()];
+
+	const authUrl = `https://www.reddit.com/api/v1/authorize?client_id=CLIENT_ID&response_type=TYPE&
+	state=RANDOM_STRING&redirect_uri=URI&scope=SCOPE_STRING`;
+
+	useHotkeys("Escape", () => {
+		userInput.current.blur();
+		setHide(true);
+	});
 
 	useEffect(() => {
-		user.current.focus(); // focusing on the form
-		const watch = (e) => e.target !== popup.current && setHide(true);
+		userInput.current.focus(); // focusing on the form
+		const watch = (e) => {
+			// we need to check path otherwise clickin a child also hides the popup.
+			!e.path.includes(popup.current) && setHide(true);
+		};
 		document.addEventListener("click", watch);
 
 		return () => {
 			document.removeEventListener("click", watch);
 		};
 	}, []);
+
+	const submit = () => {
+		// should i hide it ?
+		setHide(true);
+	};
+
+	const watchEsc = (e) => (e.key === "Escape" ? setHide(true) : null);
 
 	return (
 		<div class="login-popup" ref={popup}>
@@ -59,13 +77,18 @@ const LoginPopup = ({ setHide, submit }) => {
 				type="text"
 				className="user"
 				placeholder="username"
-				ref={user}
+				onKeyDown={watchEsc}
+				ref={userInput}
 			/>{" "}
 			<br />
 			<input
 				type="password"
 				className="pass"
 				placeholder="password"
+				onKeyDown={(e) => {
+					e.key === "Enter" && submit();
+					watchEsc(e);
+				}}
 			/>{" "}
 			<br />
 			<button onClick={submit} type="submit">
